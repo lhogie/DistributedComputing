301e7fe1fc477e2751d7e990924e41d1e68886cc
Luc Hogie <luc.hogie@cnrs.fr>
2025-10-17 11:33:25 +0200

    solve System.console() issue

--- src/distributed_computing/Main.java
+++ src/distributed_computing/Main.java
@@ -4,22 +4,36 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
         throws Exception {
         var agent = new Agent();
 
-        for (var line : Files.readAllLines(Path.of("peers.txt"))) {
+        var peerFile = Path.of("peers.txt");
+        System.out.println("loading " + peerFile.toAbsolutePath());
+
+        for (var line : Files.readAllLines(peerFile)) {
             var p = new Peer();
             p.ipAddress = InetAddress.getByName(line);
             agent.peers.add(p);
         }
 
+        System.out.println(agent.peers.size() + " peer(s) found");
+
+
+        Scanner input = new Scanner(System.in);
 
         for (;;){
-            var s = System.console().readLine();
-            agent.offer(s);
+            System.out.println("> ");
+
+            while (input.hasNextLine()) {
+                var s = input.nextLine();
+                agent.client.broadcast(s);
+            }
         }
     }
+
+
 }
